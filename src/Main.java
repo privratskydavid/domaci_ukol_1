7bc73a8fd68612c840c530de8370441a0edddaca
privratskydavid <privratsky_david@oauh.cz>
2026-09-11 19:44:12 +0200

    osobní údaje

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,12 @@ public class Main {
         System.out.println("Chodil jsem ven se svými kamarády.");
         System.out.println("Chodil jsem do autoškoly");
         System.out.println("Relaxoval jsem doma");
+
+        System.out.println("David Přívratský");
+        System.out.println("Hrubá Vrbka 56");
+        System.out.println("17 let");
+        System.out.println("725078028");
+        System.out.println("david95561@seznam.cz");
     }
 
 }
