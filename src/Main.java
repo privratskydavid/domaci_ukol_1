1cbeea4bf3d3302db8fe60a28d44fb4650883c46
privratskydavid <privratsky_david@oauh.cz>
2026-09-11 19:30:27 +0200

    5 vět co jsem dělal o prázdninách

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("O prázninách jsem chodil na brigádu.");
+        System.out.println("Chodil jsem na tréninky fotbalu.");
+        System.out.println("Chodil jsem ven se svými kamarády.");
+        System.out.println("Chodil jsem do autoškoly");
+        System.out.println("Relaxoval jsem doma");
+    }
+
+}
